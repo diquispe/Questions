0a1547be4612d5d32db18f661df992b79862b756
Diego <diquispe@gmail.com>
2020-03-01 09:40:05 -0500

    Llenado de informacion

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        /*
+         * each hace que cada ves que se itera user tambien itera una pregumta
+         * en ves de crearla por separado
+         *
+         * el metodo make almacena en memoria los registros
+         * en cambio el metodo create inserta los registos en la base de datos
+         */
+        factory(User::class, 4)->create()->each(function ($u) {
+            $u->questions()
+                ->saveMany(
+                    factory(\App\Question::class, rand(1,5))->make()
+                );
+        });
     }
 }
