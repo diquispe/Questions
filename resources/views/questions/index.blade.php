f61f41264e8a0228b6c9072de6a942e2a90f30c6
Diego <diquispe@gmail.com>
2020-03-01 13:02:09 -0500

    Agregado estilos y conteo de votos

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,8 +10,23 @@
                     <div class="card-body">
                         @foreach($questions as $question)
                             <div class="media">
+                                <div class="d-flex flex-column counters">
+                                    <div class="vote">
+                                        <b>{{ $question->votes }}</b> {{ Str::plural('vote', $question->votes) }}
+                                    </div>
+                                    <div class="status {{ $question->status }}">
+                                        <b>{{ $question->answers }}</b> {{ Str::plural('answers', $question->votes) }}
+                                    </div>
+                                    <div class="view">
+                                        {{ $question->views . "  " . Str::plural('view', $question->views) }}
+                                    </div>
+                                </div>
                                 <div class="media-body">
-                                    <h3 class="mt-0">{{ $question->title }}</h3>
+                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                    <p class="lead">
+                                        Asked by: <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                        <small class="text-muted">{{ $question->created_date }}</small>
+                                    </p>
                                     {{ Str::limit($question->body, 250)  }}
                                 </div>
                             </div>
